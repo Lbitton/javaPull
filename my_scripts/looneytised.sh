35e2a51bfda2fa440456a77a88cd575809055ef5
LBitton <Lucasbitton1@gmail.com>
2019-01-04 12:03:07 +0100

    edit looneytised.sh

--- my_scripts/looneytised.sh
+++ my_scripts/looneytised.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-sed -n  "s/theo1/Wile E. Coyote/p"
-sed -n "s/steven1/Daffy Duck/p"
-sed -n "s/arnaud1/Porky Pig/p"
-sed -n "s/pierre-jean/Marvin the Martian/p"
+sed -n "s/theo1/Wile E. Coyote/p; s/steven1/Daffy Duck/p; s/arnaud1/Porky pig/p; s/pierre-jean/Marvin the Martian/p"
