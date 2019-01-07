acc0337741bf322ca8f583785d4b7e9aa64ee0c7
LBitton <Lucasbitton1@gmail.com>
2019-01-07 11:17:19 +0100

    day03 cossToin

---
+++ day03/TossCoin/TossCoin.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.Random;
+import java.util.*;
+
+class TossCoin{
+	public static void playTossCoin(){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("What are you betting on ? heads or tails:");
+		String choice = sc.nextLine();
+		choice = choice.toLowerCase();
+		System.out.println("You choose " + choice);
+
+		String result = new String();
+		Random rand = new Random();
+		int n = rand.nextInt(2);
+		if(n == 0){
+			result = "heads";
+		}else{
+			result = "tails";
+		}
+		System.out.println("The result is " + result);
+		if(result.equals(choice)){
+			System.out.println("You won !");
+		}else{
+			System.out.println("Pay up, loser !");
+		}
+	}
+
+	public static void main(String arg[]){
+		playTossCoin();
+
+	}
+}
