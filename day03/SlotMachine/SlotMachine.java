585702d6140b9c320479a6990f4f649c56542fda
LBitton <Lucasbitton1@gmail.com>
2019-01-07 16:41:46 +0100

    final edit because frak the ascii

--- day03/SlotMachine/SlotMachine.java
+++ day03/SlotMachine/SlotMachine.java
@@ -15,6 +15,33 @@ class SlotMachine{
 		result[0] = WSM.Wheel1[n];
 		result[1] = WSM.Wheel2[m];
 		result[2] = WSM.Wheel3[o];
+		String sb0 = "";
+		String sb0bis = "";
+		String sb1 = "";
+		String sb1bis = "";
+		String sb2 = "";
+		String sb2bis = "";
+
+		if(result[0].length() == 5){
+			sb0 = " ";
+			sb0bis = " ";
+		}else if(result[0].length() == 6){
+			sb0 = " ";
+		}
+
+		if(result[1].length() == 5){
+                        sb1 = " ";
+                        sb1bis = " ";
+                }else if(result[1].length() == 6){
+                        sb1 = " ";
+                }
+
+		if(result[2].length() == 5){
+                        sb2 = " ";
+                        sb2bis = " ";
+                }else if(result[2].length() == 6){
+                        sb2 = " ";
+                }
 
 		System.out.println(result[0]);
 		System.out.println(result[1]);
@@ -34,15 +61,16 @@ class SlotMachine{
 		
 System.out.println("Gling gling gling...");
 System.out.println("  _________________________________");
-System.out.println("  |  __    __    ___  _____   __    |");
-System.out.println("  | / _\\  \\/ /   /___\\/__   \\ / _\\   |");
-System.out.println("  | \\ \\  / /   /   /   /  /\\ \\\\ \\  25|");
-System.out.println("  | _\\ \\/ /___/ \\_/   / /  \\/_\\ \\ []| __");
+System.out.println("  |  __    __          _____   __    |");
+System.out.println("  | / _\\  / /   //  // __   \\ / _\\   |");
+System.out.println("  | \\ \\  / /   //  //  / /\\ \\ \\  \\ ||");
+System.out.println("  | _\\ \\/ /___/ \\_//  / /  \\/_\\ \\ []| __");
 System.out.println("  | \\__/\\____/\\___/   \\/     \\__/ []|(__)");
+System.out.println("  |===_______===_______===_______===| ||");
 System.out.println("  |===_______===_______===_______===| ||");
 System.out.println("  ||*|       |*|       |*|       |*|| ||");
 System.out.println("  ||*| ***** |*| ***** |*| ***** |*|| ||");
-System.out.println("  ||*|"+result[0]+"|*|"+result[1]+"|*|"+result[2]+"|*|| ||");
+System.out.println("  ||*|"+ sb0 + result[0] + sb0bis +"|*|"+ sb1 + result[1] + sb1bis +"|*|"+ sb2 + result[2] + sb2bis +"|*|| ||");
 System.out.println("  ||*| ***** |*| ***** |*| ***** |*|| ||");
 System.out.println("  ||*|_______|*|_______|*|_______|*||_ /");
 System.out.println("  |===_______===_______===_______===|_/");
