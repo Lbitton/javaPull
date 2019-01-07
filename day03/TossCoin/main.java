ac572ffe2d88fbc3a06cd4f54acfebd99f5cb225
LBitton <Lucasbitton1@gmail.com>
2019-01-07 11:38:08 +0100

    TossCoin edited AGAIn

---
+++ day03/TossCoin/main.java
@@ -0,0 +1,12 @@
+
+
+class test{
+	public static void main(String arg[]){
+                TossCoin game = new TossCoin();
+                        if(game.playTossCoin() == true)
+                                System.out.println("You win !");
+                        else
+                                System.out.println("you lose, pay up !");
+
+        }
+}
